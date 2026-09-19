f7bc295e69c8c8ad9b7c90c6d67dafe729b9aba1
muradsafarli1 <149587693+muradsafarli1@users.noreply.github.com>
2026-09-19 12:21:07 +0400

    Add Firebase client configuration for Auth and Firestore

---
+++ firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjEd4pN33WpoJlT2rsAgDFMO3A4Uh-M4s",
+  authDomain: "able-2ca24.firebaseapp.com",
+  projectId: "able-2ca24",
+  storageBucket: "able-2ca24.firebasestorage.app",
+  messagingSenderId: "784635231366",
+  appId: "1:784635231366:web:fbde7a6daaf4e3e4f0e507",
+  measurementId: "G-L9C4YX8MEJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { app, auth, db, analytics };
